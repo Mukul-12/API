f171bce42bd7691f411dc116e423337a867b6762
Mukul-12 <mukulagarawal173@gmail.com>
2023-07-06 12:00:02 +0530

    Add Controller, DTO, DataLayer

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using API.Models;
+using API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]        //[Route("api/Employee")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<EmployeeDTO> getEmployees()
+        {
+            var empList = new List<EmployeeDTO>()
+            {
+                new EmployeeDTO(){Id = 1, Name = "Mukul" },
+                new EmployeeDTO(){Id = 2, Name = "Ram"}
+            };
+            return empList;
+        }
+
+    }
+}
---
+++ API/Models/DTO/EmployeeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models.DTO
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
